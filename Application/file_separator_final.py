ac57706db29b647ea5de9b8eb31668b15982c94d
LAXMAN CHANDRA RANA <laxmanchandrarana2000@gmail.com>
2022-04-30 16:56:42 +0530

    update

--- Application/file_separator_final.py
+++ Application/file_separator_final.py
@@ -1,3 +1,6 @@
+from cgitb import text
+from re import T
+from sys import flags
 import tkinter as tk
 from tkinter import RAISED, Button, Canvas, Image, Label, PhotoImage, Tk, Toplevel, YView, ttk
 from tkinter import font,colorchooser,filedialog,messagebox
@@ -27,7 +30,11 @@ browse_label=tk.Label(
     main_application,
     text='Choose a folder to organize',font='Arial 15 bold italic underline',
     foreground='Green',
-    bg='#2db2d6')
+    bg='#2db2d6',
+    highlightbackground="#5497b8",
+    highlightthickness=2,
+    bd=3
+    )
 browse_label.pack(pady= 8)
 ####### create browse button########
 
@@ -44,7 +51,10 @@ browse_button=tk.Button(
     activebackground='#0a73a1',
     activeforeground='blue',
     relief=RAISED,
-    wraplength='2c'
+    wraplength='2c',
+    highlightbackground="#5497b8",
+    highlightthickness=20,
+    bd=5
 )
 
 browse_button.bind("<Enter>", on_enter)
@@ -55,13 +65,15 @@ browse_button.pack(pady=30)
 
 def browse_func():
     feedback =filedialog.askdirectory(initialdir=os.getcwd(),title='Select Folder')
+    print(feedback)
     dict_extension={
     'Audio_extension':('.mp3','.m4a','.wav','.flac','.aac','.wav','.wma'),
     'Video_extension':('.mp4','.mov','.wmv','.avi','.avchd','.flv','.f4v','.swf','.mkv','.webm','mpeg-2'),
     'Document_extension':('.doc','.docx','.pdf','.txt','.pptx','.htm','.odt','.xls','.xlsx','.ods','.ppt','.webp'),
     'Coding_extension':('.py','.js','.css','.html','.cpp','.c','.java'),
     'Image_extension':('.jpg','.png','.jpeg','.RAW','.gif','.tiff','.psd','.eps','.ai','.ind'),
-    'Compressed_extension':('.zip','.rar')
+    'Compressed_extension':('.zip','.rar'),
+    'WindowsApp_extension':('.exe','.msi')
     }
 
     def file_finder(folder_path,file_extension):
@@ -92,10 +104,60 @@ def browse_func():
                     'ASKING',
                     'Do you Want to Create a New Folder?'
                     )
-                    if mbox2 is True:
-                        feedback2 =filedialog.askdirectory(initialdir=folder_path,title='Select Folder or Create a new Folder')
-                    elif mbox2 is False:
-                        continue
+                    flag=True
+                    while flag:
+                        if mbox2 is True:
+                            flag=True
+                            while flag:
+                                pop=Toplevel(main_application)
+                                pop.geometry("400x250")
+                                pop.configure(background="#73568f")
+
+                                f_var=tk.StringVar()
+                                f_entry=tk.Entry(
+                                    pop,
+                                    width=25,
+                                    foreground="#9e4ca6",
+                                    font='Arial 15 bold',
+                                    textvariable=f_var)
+                                f_entry.pack()
+                                f_entry.focus()
+
+                                def action():
+                                    given_f=f_var.get()
+                                    new_p=os.path.join(folder_path,given_f)
+                                    if os.path.exists(new_p):
+                                        top=Toplevel(main_application)
+                                        top.geometry("200x200")
+                                        top_label=tk.Label(top,text=f'{given_f} folder also already exists!')
+                                        top_label.pack()
+                                        top.after(2000,top.destroy)
+                                    else:
+                                        os.makedirs(new_p, exist_ok=True)
+                                        tk.Label(
+                                            pop,
+                                            text=f"{new_p} folder created succesfully!",
+                                            font='Arial 10 bold',
+                                            background='#2a62bd',
+                                            fg="White"
+                                            )
+                                        for items in file_finder(feedback,extension_tuple):
+                                            item_path2=os.path.join(feedback,items)
+                                            item_new_path2=os.path.join(new_p,items)
+                                            shutil.move(item_path2,item_new_path2)
+                                        print(new_p)
+                                        pop.after(1500,pop.destroy)
+                                submit_button=ttk.Button(
+                                    pop,
+                                    text='Submit',
+                                    command=action
+                                    )
+                                submit_button.pack()
+                                
+                                return False
+                        elif mbox2 is False:
+                            return False
+                        return False
             else:
                 os.makedirs(folder_path)
                 # created=Toplevel(main_application)
@@ -107,6 +169,7 @@ def browse_func():
                 item_path=os.path.join(feedback,items)
                 item_new_path=os.path.join(folder_path,items)
                 shutil.move(item_path,item_new_path)
+                print(item_new_path)
     else:
         return
 
